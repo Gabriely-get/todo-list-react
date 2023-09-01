0a42cfaf4213be54d054b668b8f8d42050d327a6
Gabriely-get <gabrielysantos.informatica@gmail.com>
2023-08-31 23:59:58 -0300

    Add fetch list and check items

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import { useState } from 'react';
 import * as Components from './App.styles';
+import { Item } from "./types/Item";
+import { ListItem } from './components/ListItem';
 
 const App = () => {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState<Item[]>([
+    { id: 4, name: 'Get rich', done: false },
+    {
+      id: 2,
+      name: 'Do react course',
+      done: true
+    },
+  ]);
 
   return (
       <Components.Container>
         <Components.Area>
-          <Components.Header>
-            Todo List
-          </Components.Header>
+          <Components.Header> Todo List </Components.Header>
+
+          {/* Add new todo */}
+
+          {
+            list.map((item, index) => (
+              <div>
+                <ListItem key={index} item={item} />
+              </div>
+            ))
+          }
         </Components.Area>
       </Components.Container>
   );
