9b061e9b950d73764803aa6baf7aedd4cf4627f0
Gabriely-get <gabrielysantos.informatica@gmail.com>
2023-09-02 08:29:37 -0300

    Update style for add task input

---
+++ src/components/AddArea/index.tsx
@@ -0,0 +1,11 @@
+import * as C from './styles';
+
+export const AddArea = () => {
+
+    return(
+        <C.Container>
+            <div className='image'> ➕ </div>
+            <input type='text' placeholder='Add a new task' />
+        </C.Container>
+    );
+};
